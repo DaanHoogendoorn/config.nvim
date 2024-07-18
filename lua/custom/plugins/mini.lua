4c0d09241ad497e1f50f8160ef931b637f05da51
DaanHoogendoorn <25438749+DaanHoogendoorn@users.noreply.github.com>
2024-07-18 17:36:10 +0200

    remove jump2d

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -30,9 +30,6 @@ return { -- Collection of various small independent plugins/modules
     -- hipatterns
     require('mini.hipatterns').setup()
 
-    -- jump2d
-    require('mini.jump2d').setup()
-
     -- ... and there is more!
     --  Check out: https://github.com/echasnovski/mini.nvim
   end,
