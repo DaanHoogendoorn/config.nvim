3e17ef05d88f98265ec10ef9c49b72b20bb973bb
DaanHoogendoorn <25438749+DaanHoogendoorn@users.noreply.github.com>
2024-07-31 22:43:17 +0200

    remove command line autocomplete

--- lua/custom/plugins/cmp.lua
+++ lua/custom/plugins/cmp.lua
@@ -33,8 +33,6 @@ return { -- Autocompletion
     --  into multiple repos for maintenance purposes.
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/cmp-path',
-    'hrsh7th/cmp-cmdline',
-    'hrsh7th/cmp-buffer',
   },
   config = function()
     -- See `:help cmp`
@@ -105,29 +103,6 @@ return { -- Autocompletion
         { name = 'path' },
       },
     }
-
-    -- `/` cmdline setup.
-    cmp.setup.cmdline('/', {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = {
-        { name = 'buffer' },
-      },
-    })
-
-    -- `:` cmdline setup.
-    cmp.setup.cmdline(':', {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = cmp.config.sources({
-        { name = 'path' },
-      }, {
-        {
-          name = 'cmdline',
-          option = {
-            ignore_cmds = { 'Man', '!' },
-          },
-        },
-      }),
-    })
 
     require 'custom.config.snippets'
   end,
