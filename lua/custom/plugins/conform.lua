fcd8bbb6595fe84384f76e676701a2555f33dc9c
DaanHoogendoorn <25438749+DaanHoogendoorn@users.noreply.github.com>
2024-09-18 17:06:29 +0200

    formatter updates

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -26,7 +26,7 @@ return { -- Autoformat
     end,
     formatters_by_ft = {
       lua = { 'stylua' },
-      php = { 'prettierd', 'prettier', stop_after_first = true },
+      php = { 'pint', 'php_cs_fixer', 'prettierd', 'prettier', stop_after_first = true },
       javascript = { 'prettierd', 'prettier', stop_after_first = true },
       typescript = { 'prettierd', 'prettier', stop_after_first = true },
       javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
