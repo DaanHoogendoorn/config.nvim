11fa7c8173d71f1dd8754f95f3eb4b73b569d67c
DaanHoogendoorn <25438749+DaanHoogendoorn@users.noreply.github.com>
2024-09-19 11:25:09 +0200

    use copilot.lua and add toggle

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,7 +1,33 @@
 return {
   {
-    'github/copilot.vim',
+    'zbirenbaum/copilot.lua',
     event = 'InsertEnter',
     command = 'Copilot',
+    config = function()
+      require('copilot').setup {
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          keymap = {
+            accept = false,
+          },
+        },
+      }
+
+      local suggestion = require 'copilot.suggestion'
+
+      vim.keymap.set('i', '<Tab>', function()
+        if suggestion.is_visible() then
+          suggestion.accept()
+        else
+          -- insert a tab
+          vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Tab>', true, true, true), 'n', true)
+        end
+      end, { desc = 'Accept suggestion' })
+
+      vim.keymap.set('n', '<leader>tc', function()
+        suggestion.toggle_auto_trigger()
+      end, { desc = '[T]oggle [C]opilot' })
+    end,
   },
 }
--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -1,15 +1,42 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  event = 'VimEnter',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function()
-    require('lualine').setup {
-      options = {
-        theme = 'catppuccin',
-        icons_enabled = vim.g.have_nerd_font,
-        section_separators = { left = '', right = '' },
-        component_separators = { left = '', right = '' },
-      },
-    }
-  end,
+  {
+    'nvim-lualine/lualine.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('lualine').setup {
+        options = {
+          theme = 'catppuccin',
+          icons_enabled = vim.g.have_nerd_font,
+          section_separators = { left = '', right = '' },
+          component_separators = { left = '', right = '' },
+        },
+        sections = {
+          lualine_x = {
+            {
+              'copilot',
+              symbols = {
+                status = {
+                  icons = {
+                    enabled = ' ',
+                    sleep = ' ', -- auto-trigger disabled
+                    disabled = ' ',
+                    warning = ' ',
+                    unknown = ' ',
+                  },
+                },
+              },
+            },
+            'encoding',
+            'fileformat',
+            'filetype',
+          },
+        },
+      }
+    end,
+  },
+  {
+    'AndreM222/copilot-lualine',
+    event = 'VimEnter',
+  },
 }
