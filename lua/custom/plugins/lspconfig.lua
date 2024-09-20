4457e664e2235085bf8a88fe3ccd92c268db59a0
DaanHoogendoorn <25438749+DaanHoogendoorn@users.noreply.github.com>
2024-09-20 11:03:33 +0200

    update tsserver to ts_ls

--- lua/custom/plugins/lspconfig.lua
+++ lua/custom/plugins/lspconfig.lua
@@ -146,17 +146,7 @@ return { -- LSP Configuration & Plugins
     --  - settings (table): Override the default settings passed when initializing the server.
     --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
     local servers = {
-      -- clangd = {},
-      -- gopls = {},
-      -- pyright = {},
-      -- rust_analyzer = {},
-      -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-      --
-      -- Some languages (like typescript) have entire language plugins that can be useful:
-      --    https://github.com/pmizio/typescript-tools.nvim
-      --
-      -- But for many setups, the LSP (`tsserver`) will work just fine
-      tsserver = {
+      ts_ls = {
         settings = {
           javascript = {
             inlayHints = tsserver_inlay_hints_settings,
@@ -239,7 +229,7 @@ return { -- LSP Configuration & Plugins
       'prettier',
       'prettierd',
       'phpactor',
-      'tsserver',
+      'ts_ls',
       'emmet-language-server',
       'cssls',
       'html',
