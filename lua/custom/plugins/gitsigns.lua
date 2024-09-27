3684a165ba60aedc3577a8966abb40e89def67a0
DaanHoogendoorn <25438749+DaanHoogendoorn@users.noreply.github.com>
2024-09-27 11:06:21 +0200

    use default settings for gitsigns

--- lua/custom/plugins/gitsigns.lua
+++ lua/custom/plugins/gitsigns.lua
@@ -1,13 +1,7 @@
 return { -- Adds git related signs to the gutter, as well as utilities for managing changes
   'lewis6991/gitsigns.nvim',
   event = 'BufReadPre',
-  opts = {
-    signs = {
-      add = { text = '+' },
-      change = { text = '~' },
-      delete = { text = '_' },
-      topdelete = { text = '‾' },
-      changedelete = { text = '~' },
-    },
-  },
+  config = function()
+    require('gitsigns').setup()
+  end,
 }
