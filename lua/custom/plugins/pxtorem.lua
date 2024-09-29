d9e0ce23c5ff85440c01bbdaf6743558507a3fca
DaanHoogendoorn <25438749+DaanHoogendoorn@users.noreply.github.com>
2024-09-29 13:46:58 +0200

    feat(pxtorem): enable integrations

--- lua/custom/plugins/pxtorem.lua
+++ lua/custom/plugins/pxtorem.lua
@@ -1,13 +1,11 @@
 return {
   dir = '~/Documents/Dev/px-to-rem.nvim/',
-  enabled = false,
+  dev = true,
   event = 'BufRead',
-  config = function()
-    require('px-to-rem').setup {
-      integrations = {
-        cmp = true,
-        vscode_cipchk_cssrem = true,
-      },
-    }
-  end,
+  opts = {
+    integrations = {
+      cmp = true,
+      vscode_cipchk_cssrem = true,
+    },
+  },
 }
