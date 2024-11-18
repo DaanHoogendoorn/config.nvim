f2c0de2a9f5c23a322f01c1acfb0d05f0ee7638a
DaanHoogendoorn <25438749+DaanHoogendoorn@users.noreply.github.com>
2024-11-18 19:59:44 +0100

    feat: replace cmp with blink

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -19,6 +19,39 @@ return {
     toggle = { enabled = true },
     win = { enabled = true },
     words = { enabled = true },
+    dashboard = {
+      enabled = true,
+      autokeys = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+      preset = {
+        keys = {
+          { icon = ' ', key = 'f', desc = 'Find File', action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = ' ', key = 'n', desc = 'New File', action = ':ene | startinsert' },
+          { icon = ' ', key = 'g', desc = 'Find Text', action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = ' ', key = 'r', desc = 'Recent Files', action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          { icon = ' ', key = 'c', desc = 'Config', action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+          { icon = '󰒲 ', key = 'L', desc = 'Lazy', action = ':Lazy', enabled = package.loaded.lazy },
+          { icon = ' ', key = 'q', desc = 'Quit', action = ':qa' },
+        },
+      },
+      sections = {
+        { section = 'header' },
+        { section = 'keys', gap = 1, padding = 1 },
+        { pane = 2, icon = ' ', title = 'Recent Files', section = 'recent_files', limit = 10, cwd = true, indent = 2, padding = 1 },
+        {
+          pane = 2,
+          icon = ' ',
+          title = 'Git Status',
+          section = 'terminal',
+          enabled = vim.fn.isdirectory '.git' == 1,
+          cmd = 'hub status --short --branch --renames',
+          height = 5,
+          padding = 1,
+          ttl = 5 * 60,
+          indent = 3,
+        },
+        { section = 'startup' },
+      },
+    },
   },
   keys = {
     {
