4860eaa150f27814b0714bbcaef88c06aea67326
DaanHoogendoorn <25438749+DaanHoogendoorn@users.noreply.github.com>
2024-11-22 15:04:24 +0100

    fix(copilotchat): make the autocomplete in copilot chat behave better

--- init.lua
+++ init.lua
@@ -74,6 +74,8 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+vim.opt.completeopt = 'menuone,noselect,noinsert,popup'
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
