59ac870ead465e2978cd4d7cb547b9da662ac02e
DaanHoogendoorn <25438749+DaanHoogendoorn@users.noreply.github.com>
2024-12-11 00:13:57 +0100

    feat(snacks): add zen-mode support

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -21,6 +21,7 @@ return {
     toggle = { enabled = true },
     win = { enabled = true },
     words = { enabled = true },
+    zen = { enabled = true },
     dashboard = {
       enabled = true,
       autokeys = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
@@ -92,6 +93,24 @@ return {
       end,
       desc = 'Prev Reference',
       mode = { 'n', 't' },
+    },
+    {
+      '<leader>N',
+      desc = 'Neovim News',
+      function()
+        Snacks.win {
+          file = vim.api.nvim_get_runtime_file('doc/news.txt', false)[1],
+          width = 0.6,
+          height = 0.6,
+          wo = {
+            spell = false,
+            wrap = false,
+            signcolumn = 'yes',
+            statuscolumn = ' ',
+            conceallevel = 3,
+          },
+        }
+      end,
     },
   },
   init = function()
