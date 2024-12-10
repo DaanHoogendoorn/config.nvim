fde8bda8c145b9a8072b647b0e2be73b91202a6a
DaanHoogendoorn <25438749+DaanHoogendoorn@users.noreply.github.com>
2024-12-11 00:12:42 +0100

    feat(noice): disable unnecessary features

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -3,24 +3,30 @@ return {
   event = 'VeryLazy',
   opts = {},
   dependencies = {
-    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
     'MunifTanjim/nui.nvim',
-    -- OPTIONAL:
-    --   `nvim-notify` is only needed, if you want to use the notification view.
-    --   If not available, we use `mini` as the fallback
-    'rcarriga/nvim-notify',
   },
   config = function()
     require('noice').setup {
+      cmdline = {
+        enabled = true,
+      },
+      popupmenu = {
+        enabled = true,
+      },
+      notify = {
+        enabled = false,
+      },
       lsp = {
-        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-        override = {
-          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-          ['vim.lsp.util.stylize_markdown'] = true,
-          ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
+        progress = {
+          enabled = false,
+        },
+        signature = {
+          enabled = false,
+        },
+        message = {
+          enabled = false,
         },
       },
     }
-    require('telescope').load_extension 'noice'
   end,
 }
