acadfcc06277e1e6c97c51baef6c3d54ef34e9b6
DaanHoogendoorn <25438749+DaanHoogendoorn@users.noreply.github.com>
2024-12-30 21:49:27 +0100

    refactor(mini): remove redundant modules and only use mini.files instead

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -1,16 +1,9 @@
-return { -- Collection of various small independent plugins/modules
-  'echasnovski/mini.nvim',
+return {
+  'echasnovski/mini.files',
   dependencies = {
     'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
   },
   config = function()
-    -- Add/delete/replace surroundings (brackets, quotes, etc.)
-    --
-    -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-    -- - sd'   - [S]urround [D]elete [']quotes
-    -- - sr)'  - [S]urround [R]eplace [)] [']
-    require('mini.surround').setup()
-
     local minifiles = require 'mini.files'
     minifiles.setup {
       windows = {
@@ -23,11 +16,5 @@ return { -- Collection of various small independent plugins/modules
     vim.keymap.set('n', '<leader>e', function()
       minifiles.open()
     end, { desc = '[E]xplore' })
-
-    -- hipatterns
-    require('mini.hipatterns').setup()
-
-    -- ... and there is more!
-    --  Check out: https://github.com/echasnovski/mini.nvim
   end,
 }
