913ff686a4b4653b2ec54d1a34627c527e4a48c9
DaanHoogendoorn <25438749+DaanHoogendoorn@users.noreply.github.com>
2025-02-20 10:22:58 +0100

    refactor(blink): use default keymap

--- lua/custom/plugins/blink.lua
+++ lua/custom/plugins/blink.lua
@@ -36,20 +36,7 @@ return {
       -- see the "default configuration" section below for full documentation on how to define
       -- your own keymap.
       keymap = {
-        ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
-        ['<C-e>'] = { 'hide', 'fallback' },
-        ['<C-y>'] = { 'accept', 'fallback' },
-
-        ['<C-l>'] = { 'snippet_forward', 'fallback' },
-        ['<C-h>'] = { 'snippet_backward', 'fallback' },
-
-        ['<Up>'] = { 'select_prev', 'fallback' },
-        ['<Down>'] = { 'select_next', 'fallback' },
-        ['<C-p>'] = { 'select_prev', 'fallback' },
-        ['<C-n>'] = { 'select_next', 'fallback' },
-
-        ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
-        ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+        preset = 'default',
       },
 
       completion = {
