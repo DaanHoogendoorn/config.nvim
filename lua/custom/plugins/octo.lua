28798a6149228bb71a34f294efa8970b27590ae6
DaanHoogendoorn <25438749+DaanHoogendoorn@users.noreply.github.com>
2025-03-30 21:53:14 +0200

    feat: plugin updates for 0.11

--- lua/custom/plugins/octo.lua
+++ lua/custom/plugins/octo.lua
@@ -1,5 +1,6 @@
 return {
   'pwntester/octo.nvim',
+  enabled = false, -- TODO: remove after issue has been fixed: https://github.com/pwntester/octo.nvim/issues/1010
   dependencies = {
     'nvim-lua/plenary.nvim',
     'folke/snacks.nvim',
