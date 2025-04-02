0418c929539e6e07ac9da323f1d8c39c8b07ec80
DaanHoogendoorn <25438749+DaanHoogendoorn@users.noreply.github.com>
2025-04-02 13:39:36 +0200

    feat: improve completeopt

--- init.lua
+++ init.lua
@@ -74,7 +74,7 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
-vim.opt.completeopt = 'menuone,noselect,noinsert,popup'
+vim.opt.completeopt = 'menu,noinsert,popup,fuzzy'
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
