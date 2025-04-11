fec6097f63b0c4597cfdd262a1e45783a6436449
DaanHoogendoorn <25438749+DaanHoogendoorn@users.noreply.github.com>
2025-04-11 14:46:10 +0200

    feat: use spaces instead of tabs by default

--- init.lua
+++ init.lua
@@ -26,6 +26,7 @@ vim.opt.relativenumber = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
