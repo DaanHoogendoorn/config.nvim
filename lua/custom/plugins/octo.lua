81f0f808f8860cbccc2e6cf61905ab13bdc5264d
DaanHoogendoorn <25438749+DaanHoogendoorn@users.noreply.github.com>
2025-04-29 09:03:27 +0200

    fix: enable octo again after bug has been fixed

--- lua/custom/plugins/octo.lua
+++ lua/custom/plugins/octo.lua
@@ -1,6 +1,5 @@
 return {
   'pwntester/octo.nvim',
-  enabled = false, -- TODO: remove after issue has been fixed: https://github.com/pwntester/octo.nvim/issues/1010
   dependencies = {
     'nvim-lua/plenary.nvim',
     'folke/snacks.nvim',
