43cbbf2cc9dc19c7695204b69a26f402e83a9b0d
DaanHoogendoorn <25438749+DaanHoogendoorn@users.noreply.github.com>
2025-05-06 11:14:47 +0200

    feat(mcphub): enable auto_approve

--- lua/custom/plugins/mcphub.lua
+++ lua/custom/plugins/mcphub.lua
@@ -7,6 +7,7 @@ return {
   build = 'npm install -g mcp-hub@latest',
   config = function()
     require('mcphub').setup {
+      auto_approve = true,
       extensions = {
         avante = {
           make_slash_commands = true,
