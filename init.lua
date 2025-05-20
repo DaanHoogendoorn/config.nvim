3cf45442dda807a82a7a6317437ba1659bcd828a
DaanHoogendoorn <25438749+DaanHoogendoorn@users.noreply.github.com>
2025-05-20 11:31:39 +0200

    feat: add alias for :Lazy

--- init.lua
+++ init.lua
@@ -164,6 +164,11 @@ require('lazy').setup({
   },
 })
 
+-- Add an alias for the Lazy command: `:Lazy` to `:L`
+vim.api.nvim_create_user_command('L', function()
+  vim.cmd 'Lazy'
+end, { desc = 'Lazy.nvim' })
+
 -- Enable virtual text for diagnostics
 vim.diagnostic.config { virtual_text = true }
 
