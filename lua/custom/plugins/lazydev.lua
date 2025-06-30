67374e9691fc560b3d9fa2fa66850180fd00dba6
DaanHoogendoorn <25438749+DaanHoogendoorn@users.noreply.github.com>
2025-06-30 18:55:46 +0200

    refactor: plugin cleanup

--- lua/custom/plugins/lazydev.lua
+++ lua/custom/plugins/lazydev.lua
@@ -2,12 +2,6 @@ return {
   {
     'folke/lazydev.nvim',
     ft = 'lua',
-    dependencies = {
-      {
-        'justinsgithub/wezterm-types',
-        lazy = true,
-      },
-    },
     opts = {
       library = {
         'lazy.nvim',
