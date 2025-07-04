d81ebdcc2ecade7ff742170d268d9a4c88041477
DaanHoogendoorn <25438749+DaanHoogendoorn@users.noreply.github.com>
2025-07-04 23:03:17 +0200

    refactor(lualine): remove the copilot-lualine dependency

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -1,37 +1,31 @@
 return {
-  {
-    'nvim-lualine/lualine.nvim',
-    event = 'VeryLazy',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-      require('lualine').setup {
-        options = {
-          theme = 'catppuccin',
-          icons_enabled = vim.g.have_nerd_font,
-          section_separators = { left = '', right = '' },
-          component_separators = { left = '', right = '' },
-          disabled_filetypes = {
-            statusline = {
-              'snacks_dashboard',
-            },
+  'nvim-lualine/lualine.nvim',
+  event = 'VeryLazy',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function()
+    require('lualine').setup {
+      options = {
+        theme = 'catppuccin',
+        icons_enabled = vim.g.have_nerd_font,
+        section_separators = { left = '', right = '' },
+        component_separators = { left = '', right = '' },
+        disabled_filetypes = {
+          statusline = {
+            'snacks_dashboard',
           },
         },
-        sections = {
-          lualine_x = {
-            {
-              require('lazy.status').updates,
-              cond = require('lazy.status').has_updates,
-            },
-            'encoding',
-            'fileformat',
-            'filetype',
+      },
+      sections = {
+        lualine_x = {
+          {
+            require('lazy.status').updates,
+            cond = require('lazy.status').has_updates,
           },
+          'encoding',
+          'fileformat',
+          'filetype',
         },
-      }
-    end,
-  },
-  {
-    'AndreM222/copilot-lualine',
-    event = 'VimEnter',
-  },
+      },
+    }
+  end,
 }
