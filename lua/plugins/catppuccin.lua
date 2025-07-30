ec19df7d8ab3824b3d5f759033deed74d1c0d172
DaanHoogendoorn <25438749+DaanHoogendoorn@users.noreply.github.com>
2025-07-31 00:00:19 +0200

    feat(catppuccin): enable auto integrations

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -10,49 +10,7 @@ return {
     require('catppuccin').setup {
       flavour = 'mocha',
       transparent_background = true,
-      integrations = {
-        flash = true,
-        gitsigns = true,
-        harpoon = true,
-        mason = true,
-        markdown = true,
-        noice = true,
-        which_key = true,
-        treesitter = true,
-        treesitter_context = true,
-        indent_blankline = {
-          enabled = true,
-          colored_indent_levels = true,
-        },
-        mini = {
-          enabled = true,
-        },
-        snacks = true,
-        fidget = true,
-        lsp_trouble = true,
-        blink_cmp = true,
-        native_lsp = {
-          enabled = true,
-          virtual_text = {
-            errors = { 'italic' },
-            hints = { 'italic' },
-            warnings = { 'italic' },
-            information = { 'italic' },
-            ok = { 'italic' },
-          },
-          underlines = {
-            errors = { 'underline' },
-            hints = { 'underline' },
-            warnings = { 'underline' },
-            information = { 'underline' },
-            ok = { 'underline' },
-          },
-          inlay_hints = {
-            background = true,
-          },
-        },
-        octo = true,
-      },
+      auto_integrations = true,
     }
 
     vim.cmd.colorscheme 'catppuccin'
