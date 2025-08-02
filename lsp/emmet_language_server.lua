70e1d904a1747b84eafb83ab9fbef6d094d2993e
DaanHoogendoorn <25438749+DaanHoogendoorn@users.noreply.github.com>
2025-08-02 15:27:15 +0200

    feat(lsp): update server configs

--- lsp/emmet_language_server.lua
+++ lsp/emmet_language_server.lua
@@ -1,4 +1,34 @@
+---@brief
+---
+--- https://github.com/olrtg/emmet-language-server
+---
+--- Package can be installed via `npm`:
+--- ```sh
+--- npm install -g @olrtg/emmet-language-server
+--- ```
 return {
+  cmd = { 'emmet-language-server', '--stdio' },
   single_file_support = true,
-  filetypes = { 'html', 'css', 'javascriptreact', 'typescriptreact', 'php', 'blade', 'php_only', 'twig' },
+  filetypes = {
+    'astro',
+    'css',
+    'eruby',
+    'html',
+    'htmlangular',
+    'htmldjango',
+    'javascriptreact',
+    'less',
+    'pug',
+    'sass',
+    'scss',
+    'svelte',
+    'templ',
+    'typescriptreact',
+    'vue',
+    'php',
+    'blade',
+    'php_only',
+    'twig',
+  },
+  root_markers = { '.git' },
 }
