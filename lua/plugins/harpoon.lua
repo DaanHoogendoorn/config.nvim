38683f96b4dd86ad85217b4307dd69efc520361b
DaanHoogendoorn <25438749+DaanHoogendoorn@users.noreply.github.com>
2025-08-15 20:03:50 +0200

    refactor(harpoon): move keybinds to lazy keys

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -2,34 +2,74 @@ return {
   'ThePrimeagen/harpoon',
   branch = 'harpoon2',
   dependencies = { 'nvim-lua/plenary.nvim' },
+  keys = {
+    {
+      '<leader>!',
+      function()
+        require('harpoon'):list():replace_at(1)
+      end,
+      desc = 'Harpoon: Replace 1',
+      mode = 'n',
+    },
+    {
+      '<leader>@',
+      function()
+        require('harpoon'):list():replace_at(2)
+      end,
+      desc = 'Harpoon: Replace 2',
+      mode = 'n',
+    },
+    {
+      '<leader>#',
+      function()
+        require('harpoon'):list():replace_at(3)
+      end,
+      desc = 'Harpoon: Replace 3',
+      mode = 'n',
+    },
+    {
+      '<leader>$',
+      function()
+        require('harpoon'):list():replace_at(4)
+      end,
+      desc = 'Harpoon: Replace 4',
+      mode = 'n',
+    },
+    {
+      '<leader>1',
+      function()
+        require('harpoon'):list():select(1)
+      end,
+      desc = 'Harpoon: Select 1',
+      mode = 'n',
+    },
+    {
+      '<leader>2',
+      function()
+        require('harpoon'):list():select(2)
+      end,
+      desc = 'Harpoon: Select 2',
+      mode = 'n',
+    },
+    {
+      '<leader>3',
+      function()
+        require('harpoon'):list():select(3)
+      end,
+      desc = 'Harpoon: Select 3',
+      mode = 'n',
+    },
+    {
+      '<leader>4',
+      function()
+        require('harpoon'):list():select(4)
+      end,
+      desc = 'Harpoon: Select 4',
+      mode = 'n',
+    },
+  },
   config = function()
     local harpoon = require 'harpoon'
     harpoon:setup()
-
-    vim.keymap.set('n', '<leader>!', function()
-      harpoon:list():replace_at(1)
-    end, { silent = true, desc = 'Harpoon: Replace 1' })
-    vim.keymap.set('n', '<leader>@', function()
-      harpoon:list():replace_at(2)
-    end, { silent = true, desc = 'Harpoon: Replace 2' })
-    vim.keymap.set('n', '<leader>#', function()
-      harpoon:list():replace_at(3)
-    end, { silent = true, desc = 'Harpoon: Replace 3' })
-    vim.keymap.set('n', '<leader>$', function()
-      harpoon:list():replace_at(4)
-    end, { silent = true, desc = 'Harpoon: Replace 4' })
-
-    vim.keymap.set('n', '<leader>1', function()
-      harpoon:list():select(1)
-    end, { silent = true, desc = 'Harpoon: Select 1' })
-    vim.keymap.set('n', '<leader>2', function()
-      harpoon:list():select(2)
-    end, { silent = true, desc = 'Harpoon: Select 2' })
-    vim.keymap.set('n', '<leader>3', function()
-      harpoon:list():select(3)
-    end, { silent = true, desc = 'Harpoon: Select 3' })
-    vim.keymap.set('n', '<leader>4', function()
-      harpoon:list():select(4)
-    end, { silent = true, desc = 'Harpoon: Select 4' })
   end,
 }
