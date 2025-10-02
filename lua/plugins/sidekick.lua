31f1455226637492af657e65c7791789b5d1713f
DaanHoogendoorn <25438749+DaanHoogendoorn@users.noreply.github.com>
2025-10-02 14:05:26 +0200

    feat: replace opencode.nvim with sidekick.nvim

---
+++ lua/plugins/sidekick.lua
@@ -0,0 +1,85 @@
+return {
+  'folke/sidekick.nvim',
+  opts = {
+    -- add any options here
+    cli = {
+      mux = {
+        backend = 'tmux',
+        enabled = true,
+      },
+    },
+  },
+  keys = {
+    {
+      '<C-w>',
+      function()
+        -- if there is a next edit, jump to it, otherwise apply it if any
+        if not require('sidekick').nes_jump_or_apply() then
+          return '<C-w>' -- fallback to normal behavior
+        end
+      end,
+      expr = true,
+      mode = { 'n' },
+      desc = 'Goto/Apply Next Edit Suggestion',
+    },
+    {
+      '<leader>aa',
+      function()
+        require('sidekick.cli').toggle { name = 'opencode', focus = true }
+      end,
+      mode = { 'n', 'v' },
+      desc = 'Sidekick Toggle CLI',
+    },
+    {
+      '<leader>ab',
+      function()
+        -- relative to cwd
+        local relative_file_path = vim.fn.expand '%:~:.'
+        require('sidekick.cli').ask { name = 'opencode', msg = '@' .. relative_file_path .. ' ', submit = false }
+      end,
+      mode = { 'n', 'v' },
+      desc = 'Sidekick ask buffer',
+    },
+    {
+      '<leader>as',
+      function()
+        require('sidekick.cli').select()
+      end,
+      -- Or to select only installed tools:
+      -- require("sidekick.cli").select({ filter = { installed = true } })
+      desc = 'Sidekick Select CLI',
+    },
+    {
+      '<leader>aa',
+      function()
+        require('sidekick.cli').send { selection = true }
+      end,
+      mode = { 'v' },
+      desc = 'Sidekick Send Visual Selection',
+    },
+    {
+      '<leader>ap',
+      function()
+        require('sidekick.cli').prompt()
+      end,
+      mode = { 'n', 'v' },
+      desc = 'Sidekick Select Prompt',
+    },
+    {
+      '<C-.>',
+      function()
+        require('sidekick.cli').focus()
+      end,
+      mode = { 'n', 'x', 'i', 't' },
+      desc = 'Sidekick Switch Focus',
+    },
+    {
+      '<leader>aq',
+      function()
+        require('sidekick.cli').close { name = 'opencode' }
+      end,
+      mode = { 'n' },
+      desc = 'Sidekick Close CLI',
+    },
+  },
+}
