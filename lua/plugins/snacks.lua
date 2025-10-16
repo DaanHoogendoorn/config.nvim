1526d433ce916729718c9bd4c55311d9ff013e59
DaanHoogendoorn <25438749+DaanHoogendoorn@users.noreply.github.com>
2025-10-16 11:00:51 +0200

    feat: add snacks lsp rename on oil file name change

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -388,5 +388,15 @@ return {
         Snacks.toggle.inlay_hints():map '<leader>th'
       end,
     })
+
+    -- LSP Rename on file name change
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'OilActionsPost',
+      callback = function(event)
+        if event.data.actions.type == 'move' then
+          Snacks.rename.on_rename_file(event.data.actions.src_url, event.data.actions.dest_url)
+        end
+      end,
+    })
   end,
 }
