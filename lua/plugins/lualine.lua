ba6b9eb5e32b671e2c156414566f91d4671a1dfd
DaanHoogendoorn <25438749+DaanHoogendoorn@users.noreply.github.com>
2025-10-20 10:12:55 +0200

    feat: add sidekick copilot status to lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -23,6 +23,21 @@ return {
             require('lazy.status').updates,
             cond = require('lazy.status').has_updates,
           },
+          {
+            function()
+              return ' '
+            end,
+            color = function()
+              local status = require('sidekick.status').get()
+              if status then
+                return status.kind == 'Error' and 'DiagnosticError' or status.busy and 'DiagnosticWarn' or 'Special'
+              end
+            end,
+            cond = function()
+              local status = require 'sidekick.status'
+              return status.get() ~= nil
+            end,
+          },
           'encoding',
           'fileformat',
           'filetype',
