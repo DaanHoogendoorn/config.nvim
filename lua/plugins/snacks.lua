a4ac9dde57814333175fae2ce958585f6cf3caf5
DaanHoogendoorn <25438749+DaanHoogendoorn@users.noreply.github.com>
2025-12-16 08:32:28 +0100

    fix: remove conflicting keybind

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -100,24 +100,6 @@ return {
       mode = { 'n', 't' },
     },
     {
-      '<leader>N',
-      desc = 'Neovim News',
-      function()
-        Snacks.win {
-          file = vim.api.nvim_get_runtime_file('doc/news.txt', false)[1],
-          width = 0.6,
-          height = 0.6,
-          wo = {
-            spell = false,
-            wrap = false,
-            signcolumn = 'yes',
-            statuscolumn = ' ',
-            conceallevel = 3,
-          },
-        }
-      end,
-    },
-    {
       '<leader>.',
       function()
         Snacks.scratch()
@@ -154,7 +136,7 @@ return {
       desc = 'Command History',
     },
     {
-      '<leader>n',
+      '<leader>N',
       function()
         Snacks.picker.notifications()
       end,
