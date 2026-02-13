1896119dd24ee5868ee0f4e99cd9e774093974d7
DaanHoogendoorn <25438749+DaanHoogendoorn@users.noreply.github.com>
2026-02-13 21:24:26 +0100

    feat(snacks): enable gh extension

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -15,6 +15,7 @@ return {
     quickfile = { enabled = true },
     bufdelete = { enabled = true },
     debug = { enabled = true },
+    gh = { enabled = true },
     image = { enabled = true },
     input = { enabled = true },
     lazygit = { enabled = true },
@@ -342,6 +343,34 @@ return {
         Snacks.picker.lsp_workspace_symbols()
       end,
       desc = 'LSP Workspace Symbols',
+    },
+    {
+      '<leader>gi',
+      function()
+        Snacks.picker.gh_issue()
+      end,
+      desc = 'GitHub Issues (open)',
+    },
+    {
+      '<leader>gI',
+      function()
+        Snacks.picker.gh_issue { state = 'all' }
+      end,
+      desc = 'GitHub Issues (all)',
+    },
+    {
+      '<leader>gp',
+      function()
+        Snacks.picker.gh_pr()
+      end,
+      desc = 'GitHub Pull Requests (open)',
+    },
+    {
+      '<leader>gP',
+      function()
+        Snacks.picker.gh_pr { state = 'all' }
+      end,
+      desc = 'GitHub Pull Requests (all)',
     },
   },
   init = function()
