82e71ea496d8f472fe432d2ded2b817819426165
DaanHoogendoorn <25438749+DaanHoogendoorn@users.noreply.github.com>
2026-03-13 13:21:32 +0100

    chore: update colorizer config

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -6,5 +6,24 @@ return {
     user_default_options = {
       AARRGGBB = true,
     },
+    options = {
+      parsers = {
+        css = true,
+        css_fn = true,
+        hex = { default = true },
+        css_var = {
+          enable = true,
+          parsers = {
+            css = true,
+          },
+        },
+        css_var_rgb = {
+          enable = true,
+        },
+        sass = {
+          enable = true,
+        },
+      },
+    },
   },
 }
