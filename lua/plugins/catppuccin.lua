9bbdf6c635fc453780727ddf41ae32dd5317a23e
DaanHoogendoorn <25438749+DaanHoogendoorn@users.noreply.github.com>
2026-03-13 13:11:49 +0100

    build: plugin updates

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -4,15 +4,12 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
-    -- You can configure highlights by doing something like:
-    vim.cmd.hi 'Comment gui=none'
-
     require('catppuccin').setup {
       flavour = 'mocha',
       transparent_background = true,
       auto_integrations = true,
     }
 
-    vim.cmd.colorscheme 'catppuccin'
+    vim.cmd.colorscheme 'catppuccin-nvim'
   end,
 }
