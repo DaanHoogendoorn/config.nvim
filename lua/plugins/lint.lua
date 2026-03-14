ea06ea48064c1505be1a579d576604dec563c355
DaanHoogendoorn <25438749+DaanHoogendoorn@users.noreply.github.com>
2026-03-14 20:47:54 +0100

    style: format nvim lint config

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -1,28 +1,25 @@
 return {
+  'mfussenegger/nvim-lint',
+  event = { 'BufReadPre', 'BufNewFile' },
+  config = function()
+    local lint = require 'lint'
+    lint.linters_by_ft = {
+      -- markdown = { 'markdownlint' },
+      javascript = { 'eslint' },
+      typescript = { 'eslint' },
+      typescriptreact = { 'eslint' },
+      javascriptreact = { 'eslint' },
+      php = {},
+    }
 
-  { -- Linting
-    'mfussenegger/nvim-lint',
-    event = { 'BufReadPre', 'BufNewFile' },
-    config = function()
-      local lint = require 'lint'
-      lint.linters_by_ft = {
-        -- markdown = { 'markdownlint' },
-        javascript = { 'eslint' },
-        typescript = { 'eslint' },
-        typescriptreact = { 'eslint' },
-        javascriptreact = { 'eslint' },
-        php = {},
-      }
-
-      -- Create autocommand which carries out the actual linting
-      -- on the specified events.
-      local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
-      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
-        group = lint_augroup,
-        callback = function()
-          require('lint').try_lint(nil, { ignore_errors = true })
-        end,
-      })
-    end,
-  },
+    -- Create autocommand which carries out the actual linting
+    -- on the specified events.
+    local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
+    vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+      group = lint_augroup,
+      callback = function()
+        require('lint').try_lint(nil, { ignore_errors = true })
+      end,
+    })
+  end,
 }
