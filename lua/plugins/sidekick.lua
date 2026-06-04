a9a50e93e81588a421a18678b6d734e17735c187
DaanHoogendoorn <25438749+DaanHoogendoorn@users.noreply.github.com>
2026-06-04 16:26:16 +0200

    refactor: remove copilot integration

--- lua/plugins/sidekick.lua
+++ lua/plugins/sidekick.lua
@@ -2,6 +2,9 @@ return {
   'folke/sidekick.nvim',
   opts = {
     -- add any options here
+    nes = {
+      enabled = false,
+    },
     cli = {
       mux = {
         backend = 'tmux',
@@ -18,17 +21,6 @@ return {
     },
   },
   keys = {
-    {
-      '<C-w>',
-      function()
-        -- if there is a next edit, jump to it, otherwise apply it if any
-        if not require('sidekick').nes_jump_or_apply() then
-          return
-        end
-      end,
-      mode = { 'n' },
-      desc = 'Goto/Apply Next Edit Suggestion',
-    },
     {
       '<leader>aa',
       function()
