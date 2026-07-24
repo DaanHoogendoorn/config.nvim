1fa0382072a1e560215d30a375c780fac5cd377f
DaanHoogendoorn <25438749+DaanHoogendoorn@users.noreply.github.com>
2026-07-24 13:46:08 +0200

    fix(colorizer): prevent sass overflow error during file reading

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,27 +1,18 @@
 return {
   'catgoose/nvim-colorizer.lua',
-  event = 'BufReadPre',
+  event = 'BufReadPost',
   opts = {
     filetypes = { '*' },
-    user_default_options = {
-      AARRGGBB = true,
-    },
     options = {
       parsers = {
         css = true,
         css_fn = true,
-        hex = { default = true },
-        css_var = {
-          enable = true,
-          parsers = {
-            css = true,
-          },
-        },
-        css_var_rgb = {
-          enable = true,
-        },
-        sass = {
-          enable = true,
+        css_var = { enable = true },
+        css_var_rgb = { enable = true },
+        sass = { enable = false },
+        hex = {
+          default = true,
+          aarrggbb = true,
         },
       },
     },
